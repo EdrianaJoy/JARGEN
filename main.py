74cdfdade46b986ee826dcc5b9e68dc31f5e9a06
ram4209 <alfaroabram44@gmail.com>
2025-01-16 18:32:01 +0800

    comments

--- main.py
+++ main.py
@@ -3,9 +3,27 @@ from interpreter import *
 
 if __name__ == '__main__':
     if len(argv) > 1:
-        print("\n".join([str(item) for item in parse(argv[1])]))  # Output the result of parse
+        print("\n".join([str(item) for item in parse(argv[1])]))
     else:
         print("No input provided.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # source .venv/Scripts/activate
